112d045c49c768f92791ccd853ab5b5a7d0a1693
Anushk91 <ag9542s@missouristate.edu>
2026-03-03 03:38:04 +0000

    task 4

--- src/main/cpp/iterative_strategy.cpp
+++ src/main/cpp/iterative_strategy.cpp
@@ -17,7 +17,34 @@
 
 namespace csc232::hw03
 {
-    // TODO: Task 4 - Implement me
-    auto iterative_strategy::compute( const int num ) const -> big_number { return 0; }
+    auto iterative_strategy::compute( const int num ) const -> big_number
+    {
+        if ( num < 0 )
+        {
+            throw std::invalid_argument( "num must be non-negative" );
+        }
+
+        if ( num > UPPER_LIMIT )
+        {
+            throw std::overflow_error( "requested fibonacci index exceeds 64-bit range" );
+        }
+
+        if ( num <= 1 )
+        {
+            return num;
+        }
+
+        big_number prev2 = 0;
+        big_number prev1 = 1;
+
+        for ( int i = 2; i <= num; ++i )
+        {
+            const big_number cur = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = cur;
+        }
+
+        return prev1;
+    }
 } // hw03
 // csc232
