85845b19342bd8f658da81ccec5a32c54f847807
Anushk91 <ag9542s@missouristate.edu>
2026-03-03 03:23:54 +0000

    task 1 and 2

--- src/main/cpp/naive_strategy.cpp
+++ src/main/cpp/naive_strategy.cpp
@@ -16,7 +16,28 @@
 
 namespace csc232::hw03
 {
-    // TODO: Task 1 - Implement me
-    auto naive_strategy::compute( const int num ) const -> big_number { return 0; }
+
+    
+    auto naive_strategy::compute( const int num ) const -> big_number
+    {
+        
+        if ( num < 0 )
+        {
+            throw std::invalid_argument( "num must be non-negative" );
+        }
+
+        if ( num > UPPER_LIMIT )
+        {
+            
+            throw std::overflow_error( "requested fibonacci index exceeds 64-bit range" );
+        }
+
+        if ( num <= 1 )
+        {
+            return num; // 
+        }
+
+        return compute( num - 1 ) + compute( num - 2 );
+    }
 } // hw03
 // csc232
